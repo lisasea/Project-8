053a25fa69a4be689eb1c391fc0fa7edce0bd6e3
Lisa Carbonell <morekaleplease@gmail.com>
2019-05-29 12:26:01 -0700

    routes books.js show book detail form

--- routes/books.js
+++ routes/books.js
@@ -33,12 +33,22 @@ router.post('/', function(req, res, next) { //posts a new book to the database
     });
 });
 
-
+router.get("/:id", function(req, res, next) { //books/:id - shows book detail form
+    Book.findByPk(req.params.id).then(function(book){
+        if(book) {
+            res.render("books/update-book", { book: book, title: book.title });
+        } else {
+            res.render("Book Not Found", { book: {}, title: "Book Not Found"});
+        }
+    }).catch(function(error){
+        res.send(500, error);
+    });
+});    
 /*
 XX get / - Home route should redirect to the /books route.
 XX get /books - Shows the full list of books.
 XX get /books/new - Shows the create new book form.
-post /books/new - Posts a new book to the database.
+XXpost /books/new - Posts a new book to the database.
 get /books/:id - Shows book detail form.
 post /books/:id - Updates book info in the database.
 post /books/:id/delete - Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting.
