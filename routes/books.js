39aa5f4c84fa962304ac82338d1995b0f92d9de9
Lisa Carbonell <morekaleplease@gmail.com>
2019-05-29 11:40:29 -0700

    routes books.js get new-book

--- routes/books.js
+++ routes/books.js
@@ -1,16 +1,21 @@
-var express = require('express');
+    var express = require('express');
 var router = express.Router();
 var Book = require("../models").Book;
 var Sequelize = require('sequelize');
 var Op = Sequelize.Op;
 
-router.get('/', function(req, res, next) { //Shows the full list of books
+router.get('/', function(req, res, next) { //shows the full list of books
     Book.findAll({order: [["Year", "DESC"]]}).then(function(books){
         res.render("books/index", {books: books, title: "Books"});
     }).catch(function(error){
         res.send(500,error);
     });
 });
+
+router.get('/new-book', function(req, res, next) { //shows the create new book form
+    res.render("books/new-book", {book: {}, title: "New Book"});
+});
+
 
 /*
 XX get / - Home route should redirect to the /books route.
