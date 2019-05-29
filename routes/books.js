de2bafaa5900861062fb6698fbcb5d539581e0b0
Lisa Carbonell <morekaleplease@gmail.com>
2019-05-29 13:06:37 -0700

    routes books.js post update

--- routes/books.js
+++ routes/books.js
@@ -29,27 +29,50 @@ router.post('/', function(req, res, next) { //posts a new book to the database
             throw error;
         }
     }).catch(function(error){
-        res.send(500, error);
+        res.send(500, error)
     });
 });
 
 router.get("/:id", function(req, res, next) { //books/:id - shows book detail form
     Book.findByPk(req.params.id).then(function(book){
         if(book) {
-            res.render("books/update-book", { book: book, title: book.title });
+            res.render("books/update-book", { book: book, title: book.title })
         } else {
-            res.render("Book Not Found", { book: {}, title: "Book Not Found"});
+            res.render("Book Not Found", { book: {}, title: "Book Not Found"})
         }
     }).catch(function(error){
-        res.send(500, error);
+        res.send(500, error)
     });
-});    
-/*
+});
+
+router.put("/:id", function(req, res, next) { // post /books/:id - update book info in the database
+    Book.findByPk(req.params.id).then(function(book) {
+        if(book) {
+            return book.update(req.body)
+        } else {
+            res.send(404)
+        }
+    }).then(function(book) {
+        res.redirect("/books") // ?? ("/books/")
+    }).catch(function(error) {
+        if(error.name === "SequelizeValidationError") {
+          var book = book.build(req.body);
+          book.id = req.params.id;
+          res.render("books/update-book", { book: book, title: "Update Book", errors: error.errors }) 
+        } else {
+            throw error
+        }
+    }). catch(function(error) {
+        res.send(500, error)
+    });
+});
+
+    /*
 XX get / - Home route should redirect to the /books route.
 XX get /books - Shows the full list of books.
 XX get /books/new - Shows the create new book form.
-XXpost /books/new - Posts a new book to the database.
-get /books/:id - Shows book detail form.
+XX post /books/new - Posts a new book to the database.
+XX get /books/:id - Shows book detail form.
 post /books/:id - Updates book info in the database.
 post /books/:id/delete - Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting.
 */
